2c6a19a9aff11148b7ad26ac72f532082cb5d0cc
zhangkuo <zhangk922@163.com>
2018-12-17 13:57:21 +0800

    dev

--- scripts/generateBundles.js
+++ scripts/generateBundles.js
@@ -8,26 +8,52 @@ const path = require('path');
 
 const {PAGE_PATH} = require('./appPath');
 
-let res, rej;
-const promise = new Promise((resolve, reject) => {
-  res = resolve;
-  rej = reject;
-});
-module.exports = promise;
-
 
 /**
- * 获取src/pages下的页面列表
+ * 1.获取src/pages下的页面列表
+ * 只有目录下的directory才会被当成页面，并处理成入口
+ *
+ * @return {Promise<Dirent[]>}
  * **/
-let pageList;
-fs.readdir(PAGE_PATH, { withFileTypes: true }, function(err, files){
-  if(err){
-    console.error('获取page列表失败', err);
-    process.exit(1);
-  }
-  pageList = files.filter(file => file.isDirectory());
-});
+function getPageList(){
+  return new Promise((resolve, reject)=>{
+    fs.readdir(PAGE_PATH, { withFileTypes: true }, function(err, files){
+      if(err){
+        console.error('获取page列表失败', err);
+        reject(err);
+        process.exit(1);
+      }
+      let pageList = files.filter(file => file.isDirectory());
+      resolve(pageList);
+    });
+  });
+}
 
 /**
- * 获取入口文件
+ * 2.获取入口文件
+ * @param{Dirent[]} pageList
+ * @return {Promise<object>}
  * **/
+function getEntries(pageList){
+  return new Promise((resolve, reject)=>{
+    if(pageList.length === 0){
+      reject({msg: `没有识别到pageList，无法获取入口文件`});
+      process.exit(1);
+      return;
+    }
+    let entry = {};
+    pageList.forEach(function(page){
+      let p = path.resolve(PAGE_PATH, page.name);
+      let entryList = [page.name + '.js', page.name + '.ts', 'index.js', 'index.ts'];
+      entryList.some(v => {
+        let entryPath = path.resolve(p, v);
+        if(fs.existsSync(entryPath)){
+          return entry[page.name] = entryPath;
+        }
+      });
+      resolve(entry);
+    });
+  });
+}
+
+module.exports = getPageList().then(v=>getEntries(v));
