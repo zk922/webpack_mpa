973fba16b9e2fb44360f5e5b0812f4fef2c184e1
zhangkuo <zhangk922@163.com>
2019-01-03 20:45:48 +0800

    dev

--- scripts/config_scripts/optimize.js
+++ scripts/config_scripts/optimize.js
@@ -7,15 +7,47 @@
  * @param {object} config
  * @return {object}
  * **/
+let i = 0;
+let arr=['a','b','c','d','e','f','g','h'];
 function addOptimization(config){
   config.optimization = {
     splitChunks: {
       chunks: 'all',
+      // maxSize: 500000,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
       automaticNameDelimiter: '~',
-      minChunks: 1
+      minChunks: 1,
+      name: false,
+      // name: function () {
+      //
+      //   return arr[i++];
+      // },
+      cacheGroups: {
+        vendors: {
+          chunks: 'all',
+          test: /[\\/]node_modules[\\/]/,
+          // name: 'vendors',
+          // filename: 'abv.js',
+          minChunks: 1,
+          priority: -10,
+          enforce: true
+        },
+        common: {
+          chunks: 'all',
+          test: /[\\/]public[\\/]/,
+          // name: 'vendors',
+          // filename: 'abv.js',
+          minChunks: 1,
+          priority: -10,
+          enforce: true
+        }
+      },
+
     }
+    // runtimeChunk: {
+    //   "name": "manifest"
+    // }
   };
   return config;
 }
